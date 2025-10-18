91ea5bbaaab7bdedeea1190812b5dfed91f1e007
mjhw5jnbfz-ctrl <mjhw5jnbfz@privaterelay.appleid.com>
2025-10-18 04:54:27 +0300

    Add initial script.js with player information
    Создан скрипт

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('nickname').textContent = "Ник1";
+document.getElementById('knights').textContent = 10;
+document.getElementById('points').textContent = 250;
+document.getElementById('status').textContent = "Онлайн";
+document.getElementById('mode').textContent = "В атаке";
+document.getElementById('avatar').src = "https://example.com/avatar1.png";
